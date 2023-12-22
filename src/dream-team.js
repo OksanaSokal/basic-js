735e8b23b3f7c42c856f14297a3f4efe1269f4c7
OksanaSokal <sokaloksana@gmail.com>
2023-12-22 13:38:16 +0300

    finish three tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,32 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  let name = [];
+  if (!Array.isArray(members)) return false;
+
+  members.forEach((elem) => {
+    if (typeof elem === 'string') {
+      let newName = elem.trim();
+      name.push(newName[0].toUpperCase());
+    }
+  });
+  name.sort();
+  return name.join('');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,59 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  if (typeof str !== 'string') str = String(str);
+  if (
+    options.hasOwnProperty('addition') &&
+    typeof options.addition !== 'string'
+  )
+    options.addition = String(options.addition);
+
+  if (options.separator == undefined) options.separator = '+';
+  if (options.additionSeparator == undefined) options.additionSeparator = '|';
+  if (options.repeatTimes == undefined) return str + options.addition;
+
+  let result = [];
+  let additions = [];
+  let strAdd = '';
+
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    for (let k = 1; k <= options.additionRepeatTimes; k++) {
+      additions.push(options.addition);
+      strAdd = additions.join(options.additionSeparator);
+    }
+  } else {
+    if (
+      options.hasOwnProperty('repeatTimes') &&
+      options.hasOwnProperty('addition')
+    ) {
+      strAdd = options.addition;
+    }
+  }
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    let a = str + strAdd;
+    result.push(a);
+  }
+
+  return result.join(options.separator);
+
   // remove line with error and write your code here
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
